b370001ea71d3e0d97b42befa515463165e74739
amandabhernandez <amandabhernandez@gmail.com>
2023-01-14 20:35:03 -0500

    add rstheme file

---
+++ changetheme.R
@@ -0,0 +1,15 @@
+### Author: AHz
+### Date: 2023-01-14
+### Written in: R version 4.2.1
+### Purpose: Add the make it pretty theme
+
+
+
+# uncomment and run if you make changes to the makeitpretty.rstheme file
+
+rstudioapi::applyTheme("Textmate (default)")
+rstudioapi::removeTheme("Make It Pretty")
+
+rstudioapi::addTheme("makeitpretty.rstheme", 
+                     apply = TRUE)
+
